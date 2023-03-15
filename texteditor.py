5b49aa54896307a67d2992a45fba6dc88db61024
[Students] Maksims Petunovs <12DPMPetunovs@rvt.lv>
2023-03-15 13:44:46 +0200

    Update .gitignore file

--- texteditor.py
+++ texteditor.py
@@ -5,8 +5,7 @@ class TextEditor:
     def ShowComm():
         window = tk.Tk()
         window.title("Text Editor: DEMO")
-        window.rowconfigure(0, minsize=800, weight=1)
-        window.columnconfigure(1, minsize=800, weight=1)
+        window.attributes("-fullscreen", True)
         window.mainloop()
 
 # https://realpython.com/python-gui-tkinter/#building-your-first-python-gui-application-with-tkinter
