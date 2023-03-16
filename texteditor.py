5dd647c5e2185441b8c3409064830120cb9454e0
[Students] Maksims Petunovs <12DPMPetunovs@rvt.lv>
2023-03-16 13:25:20 +0200

    Little adjustments

--- texteditor.py
+++ texteditor.py
@@ -5,48 +5,55 @@ from tkinter import filedialog
 class TextEditor:
     def ShowWindow():
 
+    # Window/Window title
+
         window = tk.Tk()
         window.withdraw()
         window.title("Text Editor: DEMO")
 
+    # Window Size
+
         # window.attributes("-fullscreen", True)
         window.state("zoomed")
 
-        # for i in range(1):
-        #     for j in range(1):
-        #         frame = tk.Frame(
-        #             master=window,
-        #             relief=tk.RAISED,
-        #             borderwidth=1
-        #         )
-        #         frame.grid(row=i, column=j)
+    # Frames
 
-        frame_01 = tk.Frame(master=window)
-        frame_01.pack(side=tk.LEFT)
+        frame = tk.Frame(master=window, bg="red", width=180, height=1080)
+        frame.pack(side=tk.LEFT, fill=tk.Y)
 
-        frame_02 = tk.Frame(master=window)
-        frame_02.pack(side=tk.RIGHT)
+        frame_01 = tk.Frame(master=window, bg="blue", height=1080, width=1800)
+        frame_01.pack(side=tk.RIGHT, fill=tk.Y)
 
-        button_open = tk.Button(text="Open File", master=frame_01)
+    # Buttons
+
+        button_open = tk.Button(text="Open File", master=frame)
         button_open.pack()
 
-        button_new = tk.Button(text="New File", master=frame_01)
+        button_new = tk.Button(text="New File", master=frame)
         button_new.pack()
 
-        button_save = tk.Button(text="Save File", master=frame_01)
+        button_save = tk.Button(text="Save File", master=frame)
         button_save.pack()
 
-        def button_open_h(event):
-            file_path = filedialog.askopenfilename()
-        button_open.bind("<Button-1>", button_open_h)
+    # TextArea
 
-        def button_new_h(event):
-            file_path = filedialog.askopenfilename()
-        button_new.bind("<Button-1>", button_new_h)
+        textarea = tk.Text(master=frame_01, width=200, height=50)
+        textarea.pack(fill=tk.Y, side=tk.RIGHT)
 
-        def button_save_h(event):
-            file_path = filedialog.askopenfilename()
-        button_save.bind("<Button-1>", button_save_h)
+    # Event Handlers
 
+        # def button_open_h(event):
+        #     file_path = filedialog.askopenfilename()
+        # button_open.bind("<Button-1>", button_open_h)
+
+        # def button_new_h(event):
+        #     file_path = filedialog.askopenfilename()
+        # button_new.bind("<Button-1>", button_new_h)
+
+        # def button_save_h(event):
+        #     file_path = filedialog.askopenfilename()
+        # button_save.bind("<Button-1>", button_save_h)
+
+    #  Close Window
 
         window.mainloop()
