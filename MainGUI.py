bc34c16e2f93ad58a24159a0b89511edc49b99e7
[Students] Maksims Petunovs <12DPMPetunovs@rvt.lv>
2023-03-22 12:50:09 +0200

    Save

--- MainGUI.py
+++ MainGUI.py
@@ -1,15 +1,57 @@
 import tkinter as tk
+from tkinter import filedialog
 
-class MainGUI:
+class GUI:
 
     def __init__(self, root):
-        self.root = root 
-        self.button = tk.Button(self.root, text="Push me", command=self.removethis)
-        self.button.pack()
-        self.frame = tk.Frame(self.root)
-        self.frame.pack()
-        self.label = tk.Label(self.frame, text="I'll be destroyed soon!")
-        self.label.pack()
+    # Attributes
+        self.root = root
+        self.root.state("zoomed")
+        self.root.title("Text Editor: DEMO")
+
+    # Frames
+
+        # Side Frame hierarchy
+
+        side_frame = tk.Frame(master=self.root, bg="red", height=1080)
+        side_frame.pack(side=tk.LEFT, fill=tk.Y)
+
+        side_frame_01 = tk.Frame(master=side_frame, bg="green")
+        side_frame_01.pack()
+
+        # Main Frame hierarchy
+
+        main_frame = tk.Frame(master=self.root, bg="blue", height=1080)
+        main_frame.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # Buttons
+
+        button_open = tk.Button(text="Open File", master=side_frame_01)
+        button_open.pack()
+
+        button_new = tk.Button(text="New File", master=side_frame_01)
+        button_new.pack()
+
+        button_save = tk.Button(text="Save File", master=side_frame_01)
+        button_save.pack()
+
+    # TextArea
+
+        textarea = tk.Text(master=main_frame)
+
+    # Event Handlers
+
+        def button_open_h(event):
+            file_path = filedialog.askopenfilename()
+        button_open.bind("<Button-1>", button_open_h)
+
+        def button_new_h(event):
+            textarea.pack(fill=tk.Y, side=tk.RIGHT)
+        button_new.bind("<Button-1>", button_new_h)
+
+        def button_save_h(event):
+            file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+        button_save.bind("<Button-1>", button_save_h)
 
     def removethis(self):
         self.frame.destroy()
--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import tkinter as tk
 import texteditor
 import MainGUI
 
-# text_editor = texteditor.TextEditor
-# text_editor.ShowWindow()
-
-
-
 root = tk.Tk()
-window = MainGUI(root)
+window = MainGUI.GUI(root)
 root.mainloop()
